a2772bf97bfe1f1309bc3ab387455f3bb4c579ef
Joanna13777 <ivansvetlov44@gmail.com>
2026-05-23 23:00:56 +0500

    Map View Controller

---
+++ MyPlaces/Location/MapViewController.swift
@@ -0,0 +1,23 @@
+//
+//  MapViewController.swift
+//  MyPlaces
+//
+//  Created by Жанна Сергеевна  on 22/05/26.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    @IBAction func closeCV() {
+        dismiss(animated: true)
+    }
+    
+    
+
+}
